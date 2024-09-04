36b5cd465ff9ace9fd4e9d8208cb5ba968b1c456
LaurieMagnin <159555919+LaurieMagnin@users.noreply.github.com>
2024-09-04 12:03:10 +0200

    Update Flower_NbVisits.R

--- Flower_NbVisits.R
+++ Flower_NbVisits.R
@@ -1,6 +1,7 @@
 visit_tree<- read.csv("~/Stage Wild Biene+ Partner/Wild Biene+Partner/Comparison exp/Data_R/visit_tree.csv", header=TRUE, sep=";")
 names(visit_tree)
 
+visit_tree <- subset(visit_tree, visit_tree$Introduced_bee != 'control' )
 #Exploring data 
 
 library(ggplot2)
@@ -10,18 +11,54 @@ ggplot(visit_tree, aes(x=Specie, y=nb.of.flower)) +
   geom_boxplot(fill="slateblue", alpha=1) + 
   xlab("Specie")
 
-########################################### Plot the number of flower per species ###########################
+########################################### Plot the number of flower per species APPLE ###########################
 
 apple_visit<-subset(visit_tree, visit_tree$Tree%in%"apple")
 apple_visit_Selected<-subset(apple_visit,apple_visit$Specie%in% c("A.mellifera", "B.terrestris","O.bicornis","O.cornuta"))
 attach(apple_visit_Selected)
 names(apple_visit_Selected)
 
+apple_visit_Selected$nb.of.flower <- as.numeric(apple_visit_Selected$nb.of.flower)
+
 P=ggplot(apple_visit_Selected, aes(x=Specie, y=nb.of.flower)) + 
   geom_boxplot(fill="slateblue", alpha=1) + scale_y_continuous(name="number of flower visited",limits=c(0,20))+ xlab("Species")+
   theme(panel.border = element_blank(),panel.background = element_blank(),axis.line.y = element_line(colour = "grey"), panel.grid.major.y = element_line(size = 0.5, linetype = 'solid',colour = "grey"))
 P+text( c(1:nlevels(apple_visit_Selected$Specie)) , P$stats[nrow(P$stats) , ]+0.5 , paste("n = ",table(apple_visit_Selected$Specie),sep=""))
 
+
 ############## Kruscal Wallis test 
 kruskal.test(nb.of.flower~Specie) # NOT significant 
 
+library (dplyr)
+
+apple_visit_Selected_AV <-apple_visit_Selected %>%                                 # Group data
+  group_by(Specie) %>%
+  dplyr::summarize(gr_sum = mean(nb.of.flower), sd = sd(nb.of.flower), n = n(),
+                   se = sd / sqrt(n)) %>% 
+  as.data.frame()
+
+########################################### Plot the number of flower per species CHERRY ###########################
+
+cherry_visit<-subset(visit_tree, visit_tree$Tree%in%"cherry")
+cherry_visit_Selected<-subset(cherry_visit,cherry_visit$Specie%in% c("A.mellifera", "B.terrestris","O.bicornis","O.cornuta"))
+attach(cherry_visit_Selected)
+names(cherry_visit_Selected)
+
+cherry_visit_Selected$nb.of.flower <- as.numeric(cherry_visit_Selected$nb.of.flower)
+
+P=ggplot(cherry_visit_Selected, aes(x=Specie, y=nb.of.flower)) + 
+  geom_boxplot(fill="slateblue", alpha=1) + scale_y_continuous(name="number of flower visited",limits=c(0,20))+ xlab("Species")+
+  theme(panel.border = element_blank(),panel.background = element_blank(),axis.line.y = element_line(colour = "grey"), panel.grid.major.y = element_line(size = 0.5, linetype = 'solid',colour = "grey"))
+P+text( c(1:nlevels(cherry_visit_Selected$Specie)) , P$stats[nrow(P$stats) , ]+0.5 , paste("n = ",table(cherry_visit_Selected$Specie),sep=""))
+
+############## Kruscal Wallis test 
+kruskal.test(nb.of.flower~Specie) # NOT significant 
+
+
+library (dplyr)
+
+cherry_visit_Selected_AV <-cherry_visit_Selected %>%                                 # Group data
+  group_by(Specie) %>%
+  dplyr::summarize(gr_sum = mean(nb.of.flower), sd = sd(nb.of.flower), n = n(),
+                   se = sd / sqrt(n)) %>% 
+  as.data.frame()
