6e12f0d5f202886c157c357e2f7853c38fec0638
LaurieMagnin <159555919+LaurieMagnin@users.noreply.github.com>
2024-11-08 08:44:12 +0100

    Update Emergence.R

--- Emergence.R
+++ Emergence.R
@@ -1,14 +1,14 @@
-setwd("C:Users/MAGNIN/Documents/Stages/Stage Wild Biene+ Partner/Wild Biene+Partner/Comparison exp")
 emergence_apple <- read.csv("~/working/Article_Osmia/emergence_apple_censored.csv", sep=";")
 View(emergence_apple)
 attach(emergence_apple)
 names(emergence_apple)
-
+library(Hmisc)
 library(ggplot2)
 library(survival)
 library(plyr)
 library(rms)
 library("survminer")
+
 
 
  #### Data exploration 
@@ -17,20 +17,20 @@ plot(survfit(Surv(Emergence,Status)~1))
 
 ########################################## Apple emergence data analysis
 
-Graph1<-ggsurvplot(model,linetype = "strata", data=emergence_apple,ylab="Emergence", xlab="Day", fun = function(x) {1 - x},legend.labs = c("O.bicornis_Ctrl", "O.bicornis_Field","O.cornuta_Ctrl","O.cornuta_Field"))
-Graph1$plot <- Graph1$plot +scale_linetype_manual(values = c(2,1,2,1)) + theme(legend.text = element_text(size = 12))
-print(Graph1)
+####Model de Kaplan meier 
 
-####Model de cox
-model<-coxph(formula = Surv(Emergence, Status) ~ Species* strata(culture)) 
+model<-survreg(formula = Surv(Emergence, Status) ~ strata(Species)* strata(experiment), dist = "logistic")
 summary(model)
 
-####Model de Kaplan meier (but not supposed to be used for multifactor analysis)
 
-model<-survreg(formula = Surv(Emergence, Status) ~ Species* experiment, dist = "logistic")
-model1<-survreg(formula = Surv(Emergence, Status) ~ strata(Species)* strata(experiment), dist = "logistic")
-summary(model1)
-anova(model1,model)
+Graph1<-ggsurvplot(model,linetype = "strata", data=emergence_cherry,ylab="Emergence", xlab="Day", fun = function(x) {1 - x},
+                   legend.labs = c("O.bicornis_Ctrl", "O.bicornis_Field","O.cornuta_Ctrl","O.cornuta_Field"), palette = c("#9FC8C8","#298C8C", '#d8a6a6','#a00000'))
+
+Graph1$plot <- Graph1$plot + scale_linetype_manual(values = c(2,1,2,1))+ theme(legend.text = element_text(size = 12))
+print(Graph1)
+
+ggsurvplot(model1,linetype = "strata", data=emergence_apple,ylab="Emergence", xlab="Day", fun = function(x) {1 - x},legend.labs = c("O.bicornis_Ctrl", "O.bicornis_Field","O.cornuta_Ctrl","O.cornuta_Field"))
+
 
 ###############################################Pair-wise comparison with selecting data to analyse
 
@@ -58,7 +58,7 @@ summary(model)
 ######For cherry
 ##############################################################################################################
 
-emergence_cherry <- read.csv("~/Stage Wild Biene+ Partner/Wild Biene+Partner/Comparison exp/Data_R/emergence_cherry_censored.csv", sep=";")
+emergence_cherry <- read.csv("~/working/Article_Osmia/emergence_cherry_censored.csv", sep=";")
 View(emergence_cherry)
 attach(emergence_cherry)
 names(emergence_cherry)
@@ -71,26 +71,17 @@ library("survminer")
 
 Statu<-1*(Emergence>0)
 
-
 plot(survfit(Surv(Emergence,Status)~1))
 
-model<-survfit(Surv(Emergence,Status)~ Species+experiment)
-Summary(model)
+model<-survreg(formula = Surv(Emergence, Status) ~ strata(Species)* strata(experiment), dist = "logistic")
+
+summary(model)
+
 Graph1<-ggsurvplot(model,linetype = "strata", data=emergence_cherry,ylab="Emergence", xlab="Day", fun = function(x) {1 - x},
-                   legend.labs = c("O.bicornis_Ctrl", "O.bicornis_Field","O.cornuta_Ctrl","O.cornuta_Field"))
+                   legend.labs = c("O.bicornis_Ctrl", "O.bicornis_Field","O.cornuta_Ctrl","O.cornuta_Field"), palette = c("#9FC8C8","#298C8C", '#d8a6a6','#a00000'))
 
 Graph1$plot <- Graph1$plot + scale_linetype_manual(values = c(2,1,2,1))+ theme(legend.text = element_text(size = 12))
 print(Graph1)
-
-model<-coxph(formula = Surv(Emergence, Status) ~ Species* experiment)
-summary(model)
-
-
-
-model<-survreg(formula = Surv(Emergence, Status) ~ strata(Species)* strata(experiment), dist = "logistic")
-model1<-survreg(formula = Surv(Emergence, Status) ~ Species* experiment, dist = "logistic")
-summary(model)
-anova(model1,model)
 
 ###############################################Pair-wise comparison
 emergence_cherry_bicornis<-subset(emergence_cherry,emergence_cherry$Species%in%"O.bicornis")
@@ -114,3 +105,4 @@ emergence_cherry_control<-subset(emergence_cherry,emergence_cherry$experiment%in
 attach(emergence_cherry_control)
 model<-survreg(formula = Surv(Emergence, Status) ~ Species, dist = "exponential")
 summary(model)
+
